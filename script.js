42f97284f750c5c053a91da11db32d92e19fe29a
Joao Souza <jvcsouza12@gmail.com>
2022-12-20 02:18:24 -0300

    front 100% pronto

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const containerData = document.querySelector('.data');
 const btnSearch = document.querySelector('.btn');
 const containerSearch = document.querySelector('.search');
 const load = document.querySelector('.load');
+const hide = document.querySelectorAll('.hide');
 
 btnSearch.addEventListener('click', (e)=>{
     e.preventDefault();
     containerSearch.style.animationName = 'up';
+
     setTimeout(()=>{
         containerData.style.display = 'flex';
-    },1000);
+    },100);
+
+    
+    setTimeout(()=>{
+        for( let i = 0; i<hide.length;i++){
+            hide[i].style.opacity = 1;
+        }
+    },700);
     
 });
